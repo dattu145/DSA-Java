e4d01211ff5525f03bfb9946fc1970dda8c00600
dattu145 <sridharreddy145233@gmail.com>
2025-12-12 17:44:25 +0530

    Added Medium/SlidingWindow/MinimumSizeSubarraySum.java

---
+++ Medium/SlidingWindow/MinimumSizeSubarraySum.java
@@ -0,0 +1,26 @@
+public class MinimumSizeSubarraySum {
+    public static int minSubLen(int[] arr, int target) {
+        int left = 0;
+        int sum = 0;
+        int minLen = Integer.MAX_VALUE;
+
+        for (int right = 0; right < arr.length; right++) {
+            sum += arr[right];
+
+            while (sum >= target) {
+                minLen = Math.min(minLen, right - left + 1);
+                sum -= arr[left];
+                left++;
+            }
+        }
+
+        return (minLen == Integer.MAX_VALUE) ? 0 : minLen;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,3,1,2,4,3};
+        int target = 8;
+
+        System.out.println(minSubLen(arr, target));
+    }
+}
